34076d89ad880d63b1c65bbd082e1e97fe3d9b69
Salama Said <71637436+Anood28@users.noreply.github.com>
2024-05-19 17:16:09 +0400

    Add files via upload

---
+++ WasteCollectionLandingPage/LandingPage/scripts.js
@@ -0,0 +1,11 @@
+﻿// scripts.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var teamMembers = document.querySelectorAll('.team-list li h3');
+
+    teamMembers.forEach(function (member) {
+        member.addEventListener('click', function () {
+            alert('You clicked on ' + member.textContent);
+        });
+    });
+});
